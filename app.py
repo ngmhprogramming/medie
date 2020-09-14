6c696a0bceb07cd40ce59664e59e13982e74fc6d
Chen Nuode <chennuode2012@gmail.com>
2020-09-14 22:54:40 +0800

    updated: :

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, session, redirect, url_for, request
 
 app = Flask(__name__, static_url_path="/static")
-app.secret_key = "mereallywannadie69420"
+app.secret_key = "mereallywannaloveAWSrn"
 
 def get_username():
     if "username" in session: return session["username"]
